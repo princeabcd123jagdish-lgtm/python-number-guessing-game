708aea981dd8b638b4427f2d965010a843299245
princeabcd123jagdish-lgtm <princeabcd123jagdish@gmail.com>
2026-07-16 03:03:38 +0530

    Implement number guessing game logic

--- NumGuess.py
+++ NumGuess.py
@@ -1,1 +1,13 @@
-
+import random
+attempts=0
+a=random.randint(1,100)
+while True:
+    attempts+=1
+    b=int(input('Enter an integer: '))
+    if b==a:
+        print(f'Congratulations🎉\nYou guessed the number in {attempts} attempts')
+        break
+    elif b<a:
+        print('Guess  higher')
+    else:
+        print ('Guess lower')
